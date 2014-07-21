1fd645418cf9e98e16a0907c6947dc01727e41f2
Aojia Zhao <aojia93@stanford.edu>
2014-07-21 01:52:16 -0500

    first comment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+	def index
+      # return all messages
+
+    end
+
+    # GET new_message_url
+    def new
+      # return an HTML form for describing a new message
+    end
+
+
+
+    # GET message_url(:id => 1)
+    def show
+      # find and return a specific message
+    end
+
+    # GET edit_message_url(:id => 1)
+    def edit
+      # return an HTML form for editing a specific message
+    end
+
+    # PUT message_url(:id => 1)
+    def update
+      # find and update a specific message
+    end
+
+
+    def create
+
+  end
+
+  def destroy
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ibay::Application.config.session_store :cookie_store, key: '_ibay_session'
